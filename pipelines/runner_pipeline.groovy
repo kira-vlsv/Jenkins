24f70e2e5effceb90348756d57690adc6246b417
kvlasova <vlsv-kira@ya.ru>
2025-02-11 09:08:02 +0100

    fixes

--- pipelines/runner_pipeline.groovy
+++ pipelines/runner_pipeline.groovy
@@ -38,32 +38,4 @@ timeout(time: 10, unit: 'MINUTES') {
 
         parallel jobs
     }
-
-    node('gradle') {
-        stage('Collect Allure Results') {
-            sh 'mkdir -p allure-results'
-
-            copyArtifacts(
-                    projectName: 'ui-tests',
-                    filter: 'build/allure-results/**',
-                    target: 'allure-results',
-                    flatten: true,
-                    allowEmpty: true
-            )
-            copyArtifacts(
-                    projectName: 'api-tests',
-                    filter: 'build/allure-results/**',
-                    target: 'allure-results',
-                    flatten: true,
-                    allowEmpty: true
-            )
-        }
-
-        stage('Generate Allure Report') {
-            sh 'ls -lah allure-results'  // Проверка наличия файлов перед генерацией отчёта
-            allure([
-                    results: [[path: 'allure-results']]
-            ])
-        }
-    }
 }
